8f510c9a39acfde60869bb2fdeb7d876c621f963
Prabeg Shakya <shakyaprabeg@gmail.com>
2026-01-22 17:36:39 +1100

    Add a logger

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
+import time
+import uuid
+
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from .api import job_scrape_router
 from .job_scrapers import ScraperRegistry, SeekJobScraper
-
+from .logger import REQUEST_ID_CTX, logger
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,5 +19,31 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.middleware("http")
+async def add_request_id(request: Request, call_next):
+    req_id = request.headers.get("X-REQUEST-ID") or str(uuid.uuid4())
+    REQUEST_ID_CTX.set(req_id)
+
+    start = time.perf_counter()
+
+    response = await call_next(request)
+    
+    duration_ms = (time.perf_counter() - start) * 1000
+
+    logger.info(
+        "HTTP Request",
+        extra={
+            "request_id": req_id,
+            "method": request.method,
+            "path": request.url.path,
+            "status_code": response.status_code,
+            "duration_ms": duration_ms,
+        }
+    )
+    response.headers["X-REQUEST-ID"] = req_id
+    return response
+
 
 app.include_router(job_scrape_router)
