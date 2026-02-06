d1c2bf02cdf160e551c373eb5305e63a83bf26b0
Prabeg Shakya <shakyaprabeg@gmail.com>
2026-02-06 11:12:01 +1100

    Add url cannonicalization to the scraper

--- src/job_scrapers.py
+++ src/job_scrapers.py
@@ -1,8 +1,15 @@
 import logging
+import re
 
 from abc import ABC, abstractmethod
-from typing import Any
-from urllib.parse import urlparse
+from typing import Any, override
+from urllib.parse import (
+    parse_qs,
+    parse_qsl,
+    urlencode,
+    urlparse,
+    urlunparse,
+)
 
 from playwright.async_api import Page, TimeoutError
 from tenacity import (
@@ -17,12 +24,71 @@ from .logger import logger
 
 
 class JobScraper(ABC):
+    def normalize(self, url: str) -> str:
+        NOISE_PARAMS = {
+            "breadcrumbs",
+            "fbclid",
+            "gclid",
+            "ref",
+            "session_id",
+            "utm_campaign",
+            "utm_content",
+            "utm_medium",
+            "utm_source",
+            "utm_term",
+        }
+
+        parsed_url = urlparse(url.strip())
+
+        scheme = parsed_url.scheme.lower()
+        netloc = parsed_url.netloc.lower()
+
+        if netloc.startswith("www."):
+            netloc = netloc[4:]
+
+        path = parsed_url.path
+
+        if len(path) > 1 and path.endswith("/"):
+            path = path.rstrip("/")
+
+        query_params = [
+            (k, v)
+            for k, v in parse_qsl(parsed_url.query)
+            if k.lower() not in NOISE_PARAMS
+        ]
+
+        query_params.sort()
+        query = urlencode(query_params)
+
+        return urlunparse((scheme, netloc, path, '', query, ''))
+
     @abstractmethod
     async def scrape(self, page) -> dict[str, Any]:
         pass
 
 
 class SeekJobScraper(JobScraper):
+    JOB_PATH_PATTERN = re.compile(r"^/job/(\d+)$")
+
+    @override
+    def normalize(self, url: str) -> str:
+        job_id = None
+
+        parsed_url = urlparse(super().normalize(url=url))
+        query_params = parse_qs(parsed_url.query)
+
+        job_id = query_params.get("jobId", None)
+
+        if match := self.JOB_PATH_PATTERN.search(parsed_url.path):
+            job_id = match.group(1)
+        elif "jobId" in query_params:
+            job_id = query_params["jobId"][0]
+        
+        if job_id:
+            return f"https://www.seek.com.au/job/{job_id}"
+
+        return urlunparse(parsed_url)
+
     @retry(
         before_sleep=before_sleep_log(logger=logger, log_level=logging.WARNING,),
         retry=retry_if_exception_type(TimeoutError),
